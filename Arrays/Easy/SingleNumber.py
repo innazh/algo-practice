0256cf1e30a0acd888f2b1f2b553594c01df335f
Inna <innazh1133@gmail.com>
2021-04-18 16:49:03 -0400

    +

--- Arrays/Easy/SingleNumber.py
+++ Arrays/Easy/SingleNumber.py
@@ -1,3 +1,28 @@
+#Note: read about bit manipulation and practice it, seems like no space linear solution is the bit manipulation one
+#Now trying for the second linear solution with no extra space.
+#Results: no explicit extra space is used, however python's sort function still uses some extra space and time under the hood
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
+        res=-1
+        nums.sort()
+        found=False
+        i=0
+        
+        while i<len(nums)-1 and not found:
+            if nums[i]!=nums[i+1]:
+                    res = nums[i]
+                    found=True
+            if not found:
+                n = nums[i]
+                while n==nums[i]:
+                    i+=1
+        if not found:
+            res = nums[len(nums)-1]
+            
+        return res
+
 #initial bruteforce-based solution using a dictionary to keep track on how many times each element is present, 
 #then iterating through the dictionary to find the element that is only encountered once
 class Solution:
