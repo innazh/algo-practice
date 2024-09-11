19f73eb815b20e7d70efc7441ff25100cdf7c060
Inna <innazh1133@gmail.com>
2024-09-11 15:25:15 -0400

    prefix subarray sum equals k

---
+++ Arrays/Medium/SubarraySumEqualsK.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        res=0
+        currSum = 0
+        prefixSums = {0:1} #how many times sums are occurring within the array
+        # as we're going through the array, we're recording the on-going running sum and all the prefix sums that we've encountered thus far.
+        # this allows us to see if we've encountered a prefix sum before, subtracting which from the current running sum, will result in the subarray
+        # that sums up to k
+        for num in nums:
+            currSum += num
+            diff = currSum - k
+
+            res += prefixSums.get(diff, 0)
+            prefixSums[currSum] = 1 + prefixSums.get(currSum, 0)
+        return res
+    
